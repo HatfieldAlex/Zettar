06fa6fed634bd763af6f7bbc97bedea190678de6
HatfieldAlex <alex.hatfield.tech@gmail.com>
2025-07-24 11:12:01 +0100

    initial location coords input template

--- zettar_prototype/location_input/views.py
+++ zettar_prototype/location_input/views.py
@@ -1,10 +1,4 @@
-from django.http import HttpResponse
+from django.shortcuts import render
 
 def location_form(request):
-    return HttpResponse("<h1>Hello! This is the location input page.</h1>")
-
-    # location = None
-    # if request.method == 'POST':
-    #     location = request.POST.get('location')
-
-    # return render(request, 'location_input/form.html', {'location': location})
+    return render(request, 'location_input/form.html')
