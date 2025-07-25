57c857cd89a38047e73aaece70dfeffa5b103b2e
HatfieldAlex <alex.hatfield.tech@gmail.com>
2025-07-25 11:15:50 +0100

    intiial map set up established

--- zettar_prototype/location_input/urls.py
+++ zettar_prototype/location_input/urls.py
@@ -3,6 +3,8 @@ from . import views
 
 urlpatterns = [
     path('', views.location_form, name='location_form'),
-    path('process-location/', views.process_location, name='process_location'), 
+    path('process-location/', views.process_location, name='process_location'),
+    path('map-visualisation/', views.map_visualisation, name='map_visualisation'),
+    path('save-location/', views.save_location, name='save_location'),
 ]
 
--- zettar_prototype/location_input/views.py
+++ zettar_prototype/location_input/views.py
@@ -3,10 +3,28 @@ from django.contrib.gis.geos import Point
 from django.contrib.gis.db.models.functions import Distance
 from .models import Substations 
 from .utils import get_osrm_driving_distance
+from django.views.decorators.csrf import csrf_exempt
+from django.http import HttpResponse
+
+def map_view(request):
+    return render(request, 'map.html')
+
+@csrf_exempt
+def save_location(request):
+    if request.method == 'POST':
+        lat = request.POST.get('latitude')
+        lng = request.POST.get('longitude')
+        print(f"Latitude: {lat}, Longitude: {lng}")
+        # Optionally save to a model here
+        return HttpResponse("Location saved successfully!")
+    return HttpResponse("Invalid request", status=400)
 
 
 def location_form(request):
     return render(request, 'location_input/form.html')
+
+def map_visualisation(request):
+    return render(request, 'location_input/map_visualisation.html')
 
 def process_location(request):
     latitude = request.POST.get('latitude')
