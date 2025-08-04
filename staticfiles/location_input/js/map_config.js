8a05954772c5132a3a58dbf433a18813bdfa68b2
HatfieldAlex <alex.hatfield.tech@gmail.com>
2025-08-04 15:40:18 +0100

    chore(gitingore): untrack ignored files and folders

--- staticfiles/location_input/js/map_config.js
+++
@@ -1,9 +0,0 @@
-export const mapOptions = {
-  zoom: 6,
-  center: { lat: 51.5, lng: -0.12 },
-  streetViewControl: false,
-  zoomControl: true,
-  rotateControl: false,
-  styles: [/* your existing styles */],
-};
-
