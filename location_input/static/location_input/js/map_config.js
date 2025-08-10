ac2f5a88e6e73b11eca52832fa85f368764610ca
HatfieldAlex <alex.hatfield.tech@gmail.com>
2025-08-10 12:59:14 +0100

    style(landing-page): shade unavailable regions on landing page map

--- location_input/static/location_input/js/map_config.js
+++ location_input/static/location_input/js/map_config.js
@@ -15,3 +15,33 @@ export function geojsonGeometryToGMapPaths(geom) {
   return rings.map(ring => ring.map(([lng, lat]) => ({ lat, lng })));
 }
 
+export function onMapClick(event) {
+  const lat = event.latLng.lat();
+  const lng = event.latLng.lng();
+  console.log("Map clicked at:", lat, lng);
+
+  if (marker) marker.setMap(null);
+  
+  marker = new google.maps.Marker({
+    position: { lat, lng },
+    map: map,
+    title: "Selected Location",
+  });
+  Alpine.store('app').location = { lat, lng };
+}
+
+export const UnavailableRegionOptions = {
+  strokeOpacity: 0,        
+  fillColor: "#e88b82ff",     
+  fillOpacity: 0.10,        
+  clickable: false,
+  zIndex: 2
+}
+
+export const AvailableRegionOptions = {
+        strokeOpacity: 0,         
+        fillColor: "#5bc4a1ff",    
+        fillOpacity: 0,       
+        clickable: true,
+        zIndex: 2
+}
--- location_input/static/location_input/js/map_script.js
+++ location_input/static/location_input/js/map_script.js
@@ -1,57 +1,9 @@
-import { mapOptions, geojsonGeometryToGMapPaths } from './map_config.js';
+import { mapOptions, geojsonGeometryToGMapPaths, UnavailableRegionOptions, AvailableRegionOptions } from './map_config.js';
 
 let map;
 let marker;
 
-
-async function initMap() {
-  map = new google.maps.Map(document.getElementById("map"), mapOptions);
-
-  try {
-    const resp = await fetch('/static/location_input/data/dno_licence_areas.geojson'); // host locally or via your CDN
-    const geojson = await resp.json();
-    const AvailableRegions = geojson.features.filter(f => f.properties?.dno === "NGED");
-    const allAvailablePaths = AvailableRegions.flatMap(r => geojsonGeometryToGMapPaths(r.geometry));
-
-    const UnavailableRegions = geojson.features.filter(f => f.properties?.dno !== "NGED");
-    const allUnavailablePaths = UnavailableRegions.flatMap(r => geojsonGeometryToGMapPaths(r.geometry));
-
-
-    AvailableRegions.forEach(r => {
-        const poly = new google.maps.Polygon({
-        paths: allAvailablePaths,
-        strokeOpacity: 0,         // no border
-        fillColor: "#5bc4a1ff",     // warm, eye-catching fill
-        fillOpacity: 0,        // see the map details underneath
-        clickable: true,
-        zIndex: 2
-        });
-        poly.setMap(map);
-        poly.addListener("click", onMapClick);
-    });
-
-    UnavailableRegions.forEach(r => {
-        const poly = new google.maps.Polygon({
-        paths: allUnavailablePaths,
-        strokeOpacity: 0,         // no border
-        fillColor: "#e88b82ff",     // warm, eye-catching fill
-        fillOpacity: 0.10,        // see the map details underneath
-        clickable: true,
-        zIndex: 2
-        });
-        poly.setMap(map);
-        poly.addListener("click", onMapClick);
-    });
-
-  } catch (e) {
-    console.error("Error occured when finding map rendering", e);
-  }
-
-  map.addListener("click", onMapClick);
-}
-
 function onMapClick(event) {
-  // Works for both polygon clicks and base map clicks
   const lat = event.latLng.lat();
   const lng = event.latLng.lng();
   console.log("Map clicked at:", lat, lng);
@@ -63,12 +15,49 @@ function onMapClick(event) {
     map: map,
     title: "Selected Location",
   });
-
-  // Update Alpine global store
   Alpine.store('app').location = { lat, lng };
 }
 
 
+async function initMap() {
+  map = new google.maps.Map(document.getElementById("map"), mapOptions);
+
+  try {
+    const resp = await fetch('/static/location_input/data/dno_licence_areas.geojson'); 
+    const geojson = await resp.json();
+    const AvailableRegions = geojson.features.filter(f => f.properties?.dno === "NGED");
+    const allAvailablePaths = AvailableRegions.flatMap(r => geojsonGeometryToGMapPaths(r.geometry));
+
+    const UnavailableRegions = geojson.features.filter(f => f.properties?.dno !== "NGED");
+    const allUnavailablePaths = UnavailableRegions.flatMap(r => geojsonGeometryToGMapPaths(r.geometry));
+
+
+    AvailableRegions.forEach(r => {
+        const poly = new google.maps.Polygon({
+        paths: allAvailablePaths,
+        ...AvailableRegionOptions
+        
+        });
+        poly.setMap(map);
+        poly.addListener("click", onMapClick);
+    });
+
+    UnavailableRegions.forEach(r => {
+        const poly = new google.maps.Polygon({
+        paths: allUnavailablePaths,
+        ...UnavailableRegionOptions
+        });
+        poly.setMap(map);
+    });
+
+
+  } catch (e) {
+    console.error(
+        `Failed to load or render map regions from /static/location_input/data/dno_licence_areas.geojson. 
+        Check that the file exists, is valid GeoJSON, and the server is serving it correctly.`, 
+   e);
+  }
+}
 
 window.initMap = initMap;
 
